788fca69780bdadd73bb78dbf809e55eda116d18
Olatunde Ibrahim <ibrahimolatunde4j@gmail.com>
2025-03-18 08:57:31 +0000

    modified sequence creator

--- src/sequence_creator.py
+++ src/sequence_creator.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7487570b3c9a1234d93e1643036c4f4a7b0da74acad86de57440b31b753b197b
-size 2247
+oid sha256:97dc8b8cd34aced9e76ca966fbe392993b2b2f9a16a6da54584090eee43e8d16
+size 2133
